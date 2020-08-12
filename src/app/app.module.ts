90f6e79f95519252928cb0b18d014ac3a7d82c28
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2020-08-12 16:21:58 -0400

    2 modules v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppComponent } from './app.component';
 import { Test2Component } from './test2/test2.component';
 import { FormsModule } from '@angular/forms';
 import { PersonaModule } from './modules/persona/persona.module';
-import { Prod1Component } from './prod1/prod1.component';
-import { Prod2Component } from './prod2/prod2.component';
-import { Prod3Component } from './prod3/prod3.component';
 import { ProductoModule } from './modules/producto/producto.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test2Component,
-    Prod1Component,
-    Prod2Component,
-    Prod3Component
+    Test2Component
   ],
   imports: [
     BrowserModule,
