0cc5af6a2225829f0f95fa14f2ce49d4a6e7ba52
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2020-08-13 15:55:17 -0400

    admin and user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Test2Component } from './test2/test2.component';
 import { FormsModule } from '@angular/forms';
 import { PersonaModule } from './modules/persona/persona.module';
 import { ProductoModule } from './modules/producto/producto.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { UserModule } from './modules/user/user.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Test2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     PersonaModule,
-    ProductoModule
+    ProductoModule,
+    AdminModule,
+    UserModule
   ],
   providers: [
 
