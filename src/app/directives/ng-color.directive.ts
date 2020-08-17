739677a0c62b688b88674849fd19c2ba47a1ff70
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2020-08-17 15:46:06 -0400

    first directive improved

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,13 +1,38 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appNgColor]'
 })
 export class NgColorDirective {
 
-  constructor(el: ElementRef) {
+  @HostListener('mouseenter') onMouseEnter() {
+    this.changeColor('yellow');
+    console.log('MOUSE ENTER');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.changeColor('green');
+    console.log('MOUSE LEAVE');
+  }
+
+  constructor(private el: ElementRef) {
     console.log('ELEMENT REF: ', el);
     el.nativeElement.style.backgroundColor = 'yellow';
   }
 
+  changeColor (color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
 }
+
+
+/*
+class Person {
+  
+  constructor (public name: string, public lastName: string){
+    this.name = name;
+    this.lastName = lastName;
+  }
+}
+*/
