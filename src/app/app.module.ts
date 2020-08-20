5657fc0f826b95baa1893bb09d85f11779704569
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2020-08-20 16:04:24 -0400

    first routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { PersonaModule } from './modules/persona/persona.module';
-import { ProductoModule } from './modules/producto/producto.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { UserModule } from './modules/user/user.module';
 import { TestPipe } from './pipes/test.pipe';
-import { ExpoPipe } from './pipes/expo.pipe';
-import { ImpurePipe } from './pipes/impure.pipe';
-import { PurePipe } from './pipes/pure.pipe';
-import {TestComponent} from './components/test.component';
 import { NgColorDirective } from './directives/ng-color.directive';
 import { NgFocusDirective } from './directives/ng-focus.directive';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { AdminComponent } from './components/admin/admin.component';
 
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestPipe,
-    ExpoPipe,
-    ImpurePipe,
-    PurePipe,
     NgColorDirective, 
-    TestComponent, NgFocusDirective
+    NgFocusDirective, 
+    HomeComponent, 
+    AboutComponent, 
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     PersonaModule,
-    ProductoModule,
-    AdminModule,
-    UserModule
+    AppRoutingModule
   ],
   providers: [
 
