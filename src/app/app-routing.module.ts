8b01e69212ea599beb5c90afdfa45dabe12b6176
mariana <luzmarianacarlo1999@gmail.com>
2020-08-21 15:43:40 -0400

    first child router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import {NgModule} from '@angular/core';
+import { Home2Component } from './components/home2/home2.component';
+import { Home1Component } from './components/home1/home1.component';
+import { NgModule, Component } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {path: 'home1', component: Home1Component},
+      {path: 'home2', component: Home2Component},
+    ]
+  },
   { path: 'admin', component: AdminComponent },
   { path: 'about', component: AboutComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,21 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Home1Component } from './components/home1/home1.component';
+import { Home2Component } from './components/home2/home2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestPipe,
-    NgColorDirective, 
-    NgFocusDirective, 
-    HomeComponent, 
-    AboutComponent, 
-    AdminComponent
+    NgColorDirective,
+    NgFocusDirective,
+    HomeComponent,
+    AboutComponent,
+    AdminComponent,
+    Home1Component,
+    Home2Component
   ],
   imports: [
     BrowserModule,
