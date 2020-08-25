d10c8db2b877bb29c1140b328a3d8cb010853525
mariana <luzmarianacarlo1999@gmail.com>
2020-08-25 16:13:33 -0400

    structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,18 @@
-import { UserModule } from './modules/user/user.module';
-import { Home2Component } from './components/home2/home2.component';
-import { Home1Component } from './components/home1/home1.component';
+
 import { NgModule, Component } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { AboutComponent } from './components/about/about.component';
+
+
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-    children: [
-      {path: 'home1', component: Home1Component},
-      {path: 'home2', component: Home2Component},
-    ]
-  },
-  { path: 'admin', component: AdminComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'list', loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule) }
 ];
 
 
+ // tslint:disable-next-line: align
  @NgModule({
    imports: [RouterModule.forRoot(routes)],
    exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { PersonaModule } from './modules/persona/persona.module';
-import { TestPipe } from './pipes/test.pipe';
 import { NgColorDirective } from './directives/ng-color.directive';
 import { NgFocusDirective } from './directives/ng-focus.directive';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { AdminComponent } from './components/admin/admin.component';
 import { AppRoutingModule } from './app-routing.module';
-import { Home1Component } from './components/home1/home1.component';
-import { Home2Component } from './components/home2/home2.component';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {MatSliderModule} from '@angular/material/slider';
@@ -22,20 +15,13 @@ import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    TestPipe,
     NgColorDirective,
-    NgFocusDirective,
-    HomeComponent,
-    AboutComponent,
-    AdminComponent,
-    Home1Component,
-    Home2Component
+    NgFocusDirective
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    PersonaModule,
     AppRoutingModule,
     NgbModule,
     MatSliderModule,
