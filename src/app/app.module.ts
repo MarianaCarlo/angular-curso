7cb4800fb9122f884a62c29436b29e36639f04bb
mariana <luzmarianacarlo1999@gmail.com>
2020-08-25 15:27:32 -0400

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -15,6 +16,9 @@ import { Home1Component } from './components/home1/home1.component';
 import { Home2Component } from './components/home2/home2.component';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+import {MatSliderModule} from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,10 +33,13 @@ import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     PersonaModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MatSliderModule,
+    MatIconModule
   ],
   providers: [
 
