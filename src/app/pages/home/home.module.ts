9620ef9cfd4cb770a4dc9c8826e7d7c07dcdfd40
mariana <luzmarianacarlo1999@gmail.com>
2020-08-26 15:13:58 -0400

    card angular material in home module

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
-
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -6,6 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  //damy data
+  products = [
+    {
+      description: 'abs',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteñas'
+    },
+    {
+      description: 'abs',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 23,
+      title: 'hamburguesas'
+    },
+    {
+      description: 'abs',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 50,
+      title: 'pizzas'
+    },
+    {
+      description: 'abs',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 15,
+      title: 'refrescos'
+    }
+  ];
 
   constructor() { }
 
